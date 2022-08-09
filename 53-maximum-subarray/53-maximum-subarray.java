7f00442fa5bdd5fa727a4bf50236d28c39f0f6ef
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-08-09 10:58:52 +0530

    Time: 1 ms (100.00%), Space: 51.5 MB (94.31%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    //KADANE'S ALGORITHM
+    public int maxSubArray(int[] nums) {
+        int ans=Integer.MIN_VALUE;
+        int currsum=0;
+        for(int i=0;i<nums.length;i++){
+            currsum+=nums[i];
+            if(currsum>ans) ans=currsum;
+            if(currsum<0) currsum=0;
+        }
+        
+        return ans;
+    }
+}
