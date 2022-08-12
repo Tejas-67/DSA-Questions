7dd8c7e89938d2ddb1d8bcd04ea4a623ba2d07da
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-08-12 19:27:22 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (82.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,14 +1,16 @@
 class Solution {
-    int arr[]=new int[45];
-    public void fill(){
-        arr[0]=1;
-        arr[1]=2;
-        for(int i=2;i<45;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-    }
     public int climbStairs(int n) {
-        fill();
-        return arr[n-1];
+        int first=1;
+        int ans=0;
+        int second=2;
+        if(n==1) return first;
+        if(n==2) return 2;
+        for(int i=3;i<=n;i++){
+            ans=first+second;
+            first=second;
+            second=ans;
+        }
+        
+        return ans;
     }
 }
