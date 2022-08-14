5ef9212e5b6dc0f046c0797226d7fc7afe9b1526
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-08-14 12:01:18 +0530

    Time: 1 ms (71.94%), Space: 41 MB (49.53%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -4,11 +4,13 @@ class Solution {
         int dp[]=new int[n+1];
         dp[0]=0;
         dp[1]=1;
+        int max=Integer.MIN_VALUE;
         for(int i=2;i<n+1;i++){
             if(i%2==0) dp[i]=dp[i/2];
             else dp[i]=dp[i/2]+dp[1+ i/2];
+            max=Math.max(dp[i], max);
         }
-        Arrays.sort(dp);
-        return dp[n];
+       // Arrays.sort(dp);
+        return max;
     }
 }
