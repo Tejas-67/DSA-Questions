af55fcc039ad8c6df304664d74fa58c1c1427679
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-08-22 19:04:14 +0530

    Time: 3 ms (82.59%), Space: 47.1 MB (77.26%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,22 +1,36 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int n=matrix.length;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int maxi = Integer.MAX_VALUE;
         
-        for(int row=1;row<n;row++){
-            matrix[row][0]=Math.min(matrix[row-1][0]+matrix[row][0], matrix[row-1][1]+matrix[row][0]);
-                
-                for(int i=1;i<n-1;i++){
-                    matrix[row][i]=Math.min(matrix[row-1][i],Math.min(matrix[row-1][i+1], matrix[row-1][i-1])) + matrix[row][i];
-                }
-            matrix[row][n-1]=Math.min(matrix[row-1][n-1], matrix[row-1][n-2]) + matrix[row][n-1];
-        }
-        int ans=Integer.MAX_VALUE;
+        int dp[][] = new int[n][m];
         
-        for(int i=0;i<n;i++){
-            ans=Math.min(ans, matrix[n-1][i]);
-            
-        }
+         for(int j=0; j<m;j++){
+	        int ans = getMaxUtil(n-1,j,m,matrix,dp);
+	        maxi = Math.min(maxi,ans);
+	    }
+	    
+	    return maxi;
+       
         
-        return ans;
     }
+    
+    public  int getMaxUtil(int i, int j, int m, int[][] matrix,int dp[][]){
+	    
+	    // Base Conditions
+	    if(j<0 || j>=m)
+	    	return (int) Math.pow(10, 9);
+	    if(i==0)
+	        return matrix[0][j];
+	    
+	    if(dp[i][j]!=0) return dp[i][j];
+	    
+	    int up = matrix[i][j] + getMaxUtil(i-1,j,m,matrix,dp);
+	    int leftDiagonal = matrix[i][j] + getMaxUtil(i-1,j-1,m,matrix,dp);
+	    int rightDiagonal = matrix[i][j] + getMaxUtil(i-1,j+1,m,matrix,dp);
+	    
+	    return dp[i][j] =  Math.min(up,Math.min(leftDiagonal,rightDiagonal));
+	    
+	}
 }
