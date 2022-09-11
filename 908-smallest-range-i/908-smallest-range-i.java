293299efd79e00a3965ff9ec6c5fe1b245f9e0c3
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-09-11 18:12:27 +0530

    Time: 2 ms (93.10%), Space: 42.8 MB (91.00%) - LeetHub

--- 908-smallest-range-i/908-smallest-range-i.java
+++ 908-smallest-range-i/908-smallest-range-i.java
@@ -1,7 +1,12 @@
 class Solution {
     public int smallestRangeI(int[] nums, int k) {
-        Arrays.sort(nums);
-        if(nums[0]+k>=nums[nums.length-1]-k) return 0;
-        return nums[nums.length-1]-nums[0]-2*k;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int ele: nums){
+            max=Math.max(max, ele);
+            min=Math.min(min, ele);
+        }
+        if(min+k>=max-k) return 0;
+        return max-min-2*k;
     }
 }
