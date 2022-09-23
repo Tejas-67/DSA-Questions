b774936817a19ff780a8640f80e96a7153d92c41
Tejas-67 <110051718+Tejas-67@users.noreply.github.com>
2022-09-23 21:10:47 +0530

    Time: 21 ms (52.03%), Space: 135.5 MB (81.20%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -2,19 +2,22 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans=new ArrayList<>();
         List<String> temp=new ArrayList<>();
-        solve(ans, s, 0, temp);
+        boolean dp[][]=new boolean[s.length()][s.length()];
+        for(boolean arr[]: dp) Arrays.fill(arr, false);
+        solve(ans, s, 0, temp, dp);
         
         return ans;
     }
     
-    public void solve(List<List<String>> ans, String s, int index, List<String> currlist){
+    public void solve(List<List<String>> ans, String s, int index, List<String> currlist, boolean dp[][]){
         
         if(index==s.length()) ans.add(new ArrayList<>(currlist));
         
         for(int i=index; i< s.length(); i++){
-            if(ispalindrome(s.substring(index, i+1))){
+            if(dp[index][i] || ispalindrome(s.substring(index, i+1))){
+                dp[index][i]=true;
                 currlist.add(s.substring(index, i+1));
-                solve(ans, s, i+1, currlist);
+                solve(ans, s, i+1, currlist, dp);
                 currlist.remove(currlist.size()-1);
             }
             
